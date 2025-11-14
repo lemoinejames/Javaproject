78f661eba60af2702b9c2477c608ccab9449c75f
lemoinejames <lemoinejames274@gmail.com>
2025-11-14 10:56:44 +0100

    Delete ProjetTennis directory

--- ProjetTennis/src/joueurs/csv.java
+++
@@ -1,4 +0,0 @@
-Nom,Prenom,AnneeNaissance,MoisNaissance,JourNaissance,Genre,Main,Taille,Poids,Sponsor,Entraineur
-Joueur_H,AutoH1,1990,1,1,HOMME,DROITIER,180,80,Sponsor,Entraineur
-Joueuse_F,AutoF1,1992,1,1,FEMME,DROITIER,170,60,Sponsor,Entraineur
-Joueur_H,AutoH2,1991,5,12,HOMME,GUAUCHER,182,78,Sponsor,Entraineur
