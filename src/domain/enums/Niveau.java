ec33d1ad4278459ff31542959ab1d74e1edf3daa
Salah Eddine <salah-eddine.boudi@student.junia.com>
2025-11-22 13:34:33 +0100

    ajouter doc java pour les enums

--- src/domain/enums/Niveau.java
+++ src/domain/enums/Niveau.java
@@ -1,5 +1,12 @@
 package domain.enums;
 
+/**
+ * Énumération représentant les différents tours d'un tournoi du Grand Chelem.
+ * Un tournoi comporte 7 tours pour 128 joueurs.
+ *
+ * @author Salah eddine & james 
+ * @version 1.0
+ */
 public enum Niveau {
     PREMIER_TOUR,
     DEUXIEME_TOUR,
